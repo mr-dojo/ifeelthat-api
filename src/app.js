ac2e383d148a6bc069c441e42f9215afc1cce2c2
Carlo Costantino <costantinoart@gmail.com>
2020-01-20 16:49:45 -1000

    Added a test watch script and added error handler to app.js

--- src/app.js
+++ src/app.js
@@ -16,4 +16,15 @@ app.get('/', (req, res) => {
   res.send(200, "Hello, boilerplate!");
 })
 
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: "Server error"}}
+  } else {
+    console.log(error)
+    response = { error: { message: error.message, error}}
+  }
+  res.status(500).json(response);
+})
+
 module.exports = app;
