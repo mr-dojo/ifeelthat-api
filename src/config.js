c43a45ad7ba732dd7431a1a7ab170883cafcb680
Carlo Costantino <costantinoart@gmail.com>
2020-03-28 13:43:03 -1000

    Added API url and database url to config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 8000,
-  CLIENT_ORIGIN: "https://ifeelthat-app.now.sh/"
+  CLIENT_ORIGIN: "https://ifeelthat-app.now.sh/",
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://carlo@localhost/ifeelthat"
 };
