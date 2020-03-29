4f388b9b3ce136e1f31942aa4608e39629f08bd6
Carlo Costantino <costantinoart@gmail.com>
2020-03-28 15:02:38 -1000

    Added DATABASE_URL config requires

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const knex = require("knex");
-const { PORT } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
