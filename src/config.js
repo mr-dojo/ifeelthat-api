6ab83e86f4ec249290b1a7ae480dc61b5e306363
Carlo Costantino <costantinoart@gmail.com>
2020-04-14 10:50:46 -1000

    Fixed cors origin header bug

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 8000,
   CLIENT_ORIGIN:
-    this.NODE_ENV === "production"
+    process.env.NODE_ENV === "production"
       ? "https://ifeelthat-app.now.sh/"
       : "http://localhost:3000/",
   DATABASE_URL:
