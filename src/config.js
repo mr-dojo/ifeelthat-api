e4b763d4c997ef9de4399417b34d3a6c2bfd492a
Carlo Costantino <costantinoart@gmail.com>
2020-04-14 11:05:58 -1000

    Updated CLIENT_ORIGIN url

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   CLIENT_ORIGIN:
     process.env.NODE_ENV === "production"
-      ? "https://ifeelthat-app.now.sh/"
+      ? "https://ifeelthat-app.now.sh"
       : "http://localhost:3000/",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://carlo@localhost/ifeelthat",
