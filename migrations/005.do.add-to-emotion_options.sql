4e36556a34d3a4b66e64161aa6bdd940c026b729
Carlo Costantino <costantinoart@gmail.com>
2020-04-20 17:47:21 -1000

    Added more emotions to emotion_options type

---
+++ migrations/005.do.add-to-emotion_options.sql
@@ -0,0 +1,8 @@
+ALTER TYPE emotion_options ADD VALUE 'Contentment';
+ALTER TYPE emotion_options ADD VALUE 'Shame';
+ALTER TYPE emotion_options ADD VALUE 'Loneliness';
+ALTER TYPE emotion_options ADD VALUE 'Pride';
+ALTER TYPE emotion_options ADD VALUE 'Power';
+ALTER TYPE emotion_options ADD VALUE 'Confusion';
+ALTER TYPE emotion_options ADD VALUE 'Nothing';
+ALTER TYPE emotion_options ADD VALUE 'Disappointment';
